687613c6e08eec26357c4688a1b47ac86ecb34ec
Joni Laukka <jonilaukka@hotmail.com>
2023-02-27 23:37:18 +0200

    Card search collection changed to Incremental loading collection

--- ViewModels/MTGSearchViewModel.cs
+++ ViewModels/MTGSearchViewModel.cs
@@ -1,26 +1,53 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Common.Collections;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.WinUI;
 using MTGApplication.Interfaces;
 using MTGApplication.Models;
-using System.Collections.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MTGApplication.ViewModels
 {
-    public partial class MTGSearchViewModel : ObservableObject
+  public partial class MTGSearchViewModel : ObservableObject
   {
+    public class MTGCardSource : IIncrementalSource<MTGCardViewModel>
+    {
+      private readonly MTGCard[] cardArray;
+
+      public MTGCardSource() { cardArray = Array.Empty<MTGCard>(); }
+      public MTGCardSource(MTGCard[] cardArray)
+      {
+        this.cardArray = cardArray;
+      }
+
+      public async Task<IEnumerable<MTGCardViewModel>> GetPagedItemsAsync(int pageIndex, int pageSize, CancellationToken cancellationToken = default)
+      {
+        var result = await Task.Run(() => (from p in cardArray select p).Skip(pageIndex * pageSize).Take(pageSize).Select(x => new MTGCardViewModel(x)));
+
+        return result;
+      }
+    }
+
     public MTGSearchViewModel(ICardAPI<MTGCard> cardAPI)
     {
       this.cardAPI = cardAPI;
     }
 
     private readonly ICardAPI<MTGCard> cardAPI;
-    
+
     [ObservableProperty]
     private bool isBusy;
+    [ObservableProperty]
+    private IncrementalLoadingCollection<MTGCardSource, MTGCardViewModel> searchCards = new();
+    [ObservableProperty]
+    private int totalCardCount;
 
-    public ObservableCollection<MTGCardViewModel> SearchCards { get; set; } = new();
     public string SearchQuery { get; set; }
+
 
     /// <summary>
     /// Fetches cards from API using selected query, and replaces current cards with the fetched cards
@@ -29,12 +56,9 @@ namespace MTGApplication.ViewModels
     public async Task SearchSubmit()
     {
       IsBusy = true;
-      SearchCards.Clear();
       var cards = await Task.Run(() => cardAPI.FetchCards(SearchQuery));
-      foreach (var item in cards)
-      {
-        SearchCards.Add(new(item));
-      }
+      SearchCards = new(new MTGCardSource(cards));
+      TotalCardCount = cards.Length;
       IsBusy = false;
     }
   }
