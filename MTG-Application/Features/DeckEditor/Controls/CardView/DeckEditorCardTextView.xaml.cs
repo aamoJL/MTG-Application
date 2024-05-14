eb566ef5f3070fd8cfc879ea6248eb6b71cc7262
Joni Laukka <jonilaukka@hotmail.com>
2024-05-14 19:35:52 +0300

    Styling

--- MTG-Application/Features/DeckEditor/Controls/CardView/DeckEditorCardTextView.xaml.cs
+++ MTG-Application/Features/DeckEditor/Controls/CardView/DeckEditorCardTextView.xaml.cs
@@ -1,5 +1,48 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
 namespace MTGApplication.Features.DeckEditor;
 public sealed partial class DeckEditorCardTextView : DeckEditorCardViewBase
 {
+  public static readonly DependencyProperty SetIconVisibleProperty =
+      DependencyProperty.Register(nameof(SetIconVisible), typeof(bool), typeof(DeckEditorCardTextView), new PropertyMetadata(true));
+
+  public static readonly DependencyProperty TypeLineVisibleProperty =
+      DependencyProperty.Register(nameof(TypeLineVisible), typeof(bool), typeof(DeckEditorCardTextView), new PropertyMetadata(true));
+
   public DeckEditorCardTextView() => InitializeComponent();
+
+  public bool SetIconVisible
+  {
+    get => (bool)GetValue(SetIconVisibleProperty);
+    set => SetValue(SetIconVisibleProperty, value);
+  }
+  public bool TypeLineVisible
+  {
+    get => (bool)GetValue(TypeLineVisibleProperty);
+    set => SetValue(TypeLineVisibleProperty, value);
+  }
+}
+
+public class BoolToDoubleConverter : IValueConverter
+{
+  public object Convert(object value, Type targetType, object parameter, string language)
+  {
+    if (value is true) return double.Parse((string)parameter);
+    return (double)0;
+  }
+
+  public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
+}
+
+public class BoolToAutoOrGridLengthConverter : IValueConverter
+{
+  public object Convert(object value, Type targetType, object parameter, string language)
+  {
+    if (value is true) return new GridLength(double.Parse((string)parameter), GridUnitType.Pixel);
+    return new GridLength(1, GridUnitType.Auto);
+  }
+
+  public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
 }
