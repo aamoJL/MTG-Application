bd5741ea271b135a9433015d572bcc3a6d21e076
Joni Laukka <jonilaukka@hotmail.com>
2024-05-24 16:38:12 +0300

    Deck price unit tests

---
+++ MTG-ApplicationTests/GeneralTests/ModelTests/CardDeckTests/CardDeckTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MTGApplication.General.Models.CardDeck;
+using MTGApplicationTests.TestUtility.Mocker;
+
+namespace MTGApplicationTests.GeneralTests.ModelTests.CardDeckTests;
+[TestClass]
+public class CardDeckTests
+{
+  [TestMethod]
+  public void DeckPrice_HasCards_PriceIsCardPriceMultipliedByCount()
+  {
+    var deck = new MTGCardDeck()
+    {
+      DeckCards = [
+        MTGCardModelMocker.CreateMTGCardModel(count: 1, price: 1),
+        MTGCardModelMocker.CreateMTGCardModel(count: 2, price: 2),
+        ],
+    };
+
+    Assert.AreEqual(deck.DeckCards.Sum(x => x.Info.Price * x.Count), deck.DeckPrice);
+  }
+
+  [TestMethod]
+  public void DeckPrice_HasCommander_PriceIsCommanderPrice()
+  {
+    var deck = new MTGCardDeck()
+    {
+      Commander = MTGCardModelMocker.CreateMTGCardModel(count: 2, price: 8),
+    };
+
+    Assert.AreEqual(deck.Commander.Info.Price, deck.DeckPrice);
+  }
+
+  [TestMethod]
+  public void DeckPrice_HasPartner_PriceIsPartnerPrice()
+  {
+    var deck = new MTGCardDeck()
+    {
+      CommanderPartner = MTGCardModelMocker.CreateMTGCardModel(count: 2, price: 8),
+    };
+
+    Assert.AreEqual(deck.CommanderPartner.Info.Price, deck.DeckPrice);
+  }
+}
