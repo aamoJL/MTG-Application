0aeb8678e70e2ed19572802a3d479924fd34ffb7
Joni Laukka <jonilaukka@hotmail.com>
2024-06-07 16:45:49 +0300

    Combobox collection selection added to CardCollectionPage

--- MTG-Application/Features/CardCollection/CardCollectionPage.xaml.cs
+++ MTG-Application/Features/CardCollection/CardCollectionPage.xaml.cs
@@ -1,5 +1,7 @@
 using Microsoft.UI.Xaml.Controls;
+using MTGApplication.General.Models.CardCollection;
 using MTGApplication.General.Services.NotificationService;
+using System.Linq;
 
 namespace MTGApplication.Features.CardCollection;
 public sealed partial class CardCollectionPage : Page
@@ -13,4 +15,11 @@ public sealed partial class CardCollectionPage : Page
   }
 
   public CardCollectionViewModel ViewModel { get; } = new(App.MTGCardAPI);
+
+  private async void CollectionListComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+  {
+    if (e.AddedItems.FirstOrDefault() is MTGCardCollectionList list)
+      if (ViewModel.SelectListCommand.CanExecute(list.Name))
+        await ViewModel.SelectListCommand.ExecuteAsync(list.Name);
+  }
 }
