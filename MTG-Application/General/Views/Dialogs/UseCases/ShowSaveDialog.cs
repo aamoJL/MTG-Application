10df0e406259bc0c2bf809349b009485d2272ee5
Joni Laukka <jonilaukka@hotmail.com>
2024-06-29 19:11:47 +0300

    TextBoxDialog

--- MTG-Application/General/Views/Dialogs/UseCases/ShowSaveDialog.cs
+++ MTG-Application/General/Views/Dialogs/UseCases/ShowSaveDialog.cs
@@ -1,18 +1,17 @@
-﻿using System.IO;
+﻿using MTGApplication.General.Views.Dialogs.Controls;
+using System.IO;
 using System.Threading.Tasks;
 using static MTGApplication.General.Services.ConfirmationService.DialogService;
 
 namespace MTGApplication.General.Views.Dialogs.UseCases;
 
-public class ShowSaveDialog : ShowDialogUseCase<string, string>
+public class ShowSaveDialog(DialogWrapper dialogWrapper) : ShowDialogUseCase<string, string>(dialogWrapper)
 {
-  public ShowSaveDialog(DialogWrapper dialogWrapper) : base(dialogWrapper) { }
-
-  protected override async Task<string> ShowDialog(string title, string message, string data) => await new TextBoxDialog(title)
-  {
-    InvalidInputCharacters = Path.GetInvalidFileNameChars(),
-    TextInputText = data,
-    PrimaryButtonText = "Save",
-    SecondaryButtonText = string.Empty
-  }.ShowAsync(DialogWrapper);
+  protected override async Task<string> ShowDialog(string title, string message, string data)
+    => await DialogWrapper.ShowAsync(new TextBoxDialog(title)
+    {
+      InvalidInputCharacters = Path.GetInvalidFileNameChars(),
+      InputText = data,
+      PrimaryButtonText = "Save",
+    });
 }
